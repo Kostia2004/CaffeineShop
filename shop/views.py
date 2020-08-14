b37d16cbbac941adfbe2d9771d59d5dca7844cd0
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-08-14 05:26:31 +0300

    create login page

--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request=request, template_name="shop/index.html")
+
+def login(request):
+    return render(request=request, template_name="shop/login.html")
